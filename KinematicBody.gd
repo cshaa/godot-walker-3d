c74d4af7992268402b889bed4200be36ce5bfc8f
Michal Grno <m93a.cz@gmail.com>
2020-01-23 19:05:02 +0100

    walker

--- KinematicBody.gd
+++ KinematicBody.gd
@@ -1,30 +1,39 @@
 extends KinematicBody
 
-# https://docs.godotengine.org/en/3.1/classes/class_kinematicbody.html
+# Najdi uzel CameraRoot
+onready var camera = get_node("CameraRoot")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
+# Schovej kurzor a zachytávej myš
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta):
-#	self.move_and_collide( Vector3(0,0,-0.1) )
 
+# Pohyb hlavní postavy
+func _process(delta):
+	
+	var forward = self.transform.basis.xform(Vector3(0,0,-0.1))
+	var right = self.transform.basis.xform(Vector3(+0.1,0,0))
+	
+	if Input.is_action_pressed("walk_forward"):
+		self.move_and_collide( forward )
+	
+	if Input.is_action_pressed("walk_back"):
+		self.move_and_collide( -forward )
+	
+	if Input.is_action_pressed("strafe_right"):
+		self.move_and_collide( right )
+	
+	if Input.is_action_pressed("strafe_left"):
+		self.move_and_collide( -right )
+
+
+
+
+# Otáčení kamery
 func _input(event):
-	if event is InputEventKey:
-		
-		if event.scancode == KEY_UP:
-			self.move_and_collide( Vector3(0,0,-0.1) )
-		
-		if event.scancode == KEY_DOWN:
-			self.move_and_collide( Vector3(0,0,+0.1) )
-		
-		if event.scancode == KEY_LEFT:
-			self.move_and_collide( Vector3(-0.1,0,0) )
-		
-		if event.scancode == KEY_RIGHT:
-			self.move_and_collide( Vector3(+0.1,0,0) )
+	if event is InputEventMouseMotion:
+		var delta = event.relative
+		camera.rotation_degrees[0] -= delta[1] * 0.1
+		self.rotation_degrees[1] -= delta[0] * 0.1
+
